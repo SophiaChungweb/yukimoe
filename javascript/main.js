ec3abf51987da47e33079048b50647284774fcf5
SophiaChungweb <yukimoe728@gmail.com>
2022-01-09 11:12:08 +0800

    20220109_條件判斷 if... else 單一條件

--- javascript/main.js
+++ javascript/main.js
@@ -17,7 +17,31 @@ console.log(one);
 console.log(ONE);
 console.log("02");
 
+
+// 日期函數 Date 產生新的 Date
+// var year = new.Date().year;
+
+
 // 年份變數
-var year = "2022";
+// var year = "2022";
 // 由html顯示資料
-document.getElementById('year').textContent = year;
+// document.getElementById('year').textContent = year;
+
+// 條件判斷 if... else 單一條件
+var number = 12;
+// 顯示條件成立，執行的資料
+if ( number < 5) {
+    console.log("數字小於5");
+}
+
+if ( number > 10 ) console.log("數字大於 10");
+
+if ( number > 10 )
+    console.log("數字為："+number);
+
+// 顯示條件成立，執行的資料，或是不成立時，執行的資料
+// if () {
+
+// } else {
+
+// }
