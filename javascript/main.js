0bd20bc84f6c00484d96665725dd5eb21f1aa052
SophiaChungweb <yukimoe728@gmail.com>
2022-01-09 16:09:22 +0800

    20220109_迴圈  while (條件) {程式} 與 for 的差別，在於無法指定執行圈數

--- javascript/main.js
+++ javascript/main.js
@@ -111,7 +111,21 @@ for ( i = 0; i <= 10; i = i + 1) {
     console.log(i);
 }
 
-login();
+// 迴圈  while (條件) {程式} 與 for 的差別，在於無法指定執行圈數
+function whilecode () {
+    // 總數
+    let number = 200;
+
+    // 亂數 (系統產生介於 0 ~ 1 之間)，將小數變為整數 ( 數值要介於 0 ~  含30 ，需乘以 31)，設定為無條件捨去法
+    
+    let attack;
+    attack = Math.floor(Math.random() * 30);
+    // console.log(attack);
+
+}
+
+
+// login();
 
 function login() {
     // 變數；承接使用者輸入的資料
