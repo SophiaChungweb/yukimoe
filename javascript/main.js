d8758b2c0e998760e25eaa91d763d25c0e784505
SophiaChungweb <yukimoe728@gmail.com>
2022-01-09 10:12:56 +0800

    20220109_宣告變數及賦予值

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,18 @@
 // 瀏覽器的控制台 列印語法
 /* 雙引號(")和單引號(')為字串，看習慣，兩者皆可，有人說專業用雙引號*/
-console.log(1);
-console.log('01');
+
+// 變數 命名：英文開頭，英文與數字，底線，javascript的關鍵字不能設定，英文可設定大小寫混用
+// es5 版本 var
+// 宣告變數
+var one;
+var ONE;
+var One;
+var O_ne;
+var O_01ne;
+
+// 賦予 值
+one = 1;
+ONE = "01";
+console.log(one);
+console.log(ONE);
 console.log("02");
