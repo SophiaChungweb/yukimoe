f5b4a66f0cfd041789ae13b881982e038aa24203
SophiaChungweb <yukimoe728@gmail.com>
2022-01-09 17:09:47 +0800

    20220109_亂數的變數計算

--- javascript/main.js
+++ javascript/main.js
@@ -112,6 +112,8 @@ for ( i = 0; i <= 10; i = i + 1) {
 }
 
 // 迴圈  while (條件) {程式} 與 for 的差別，在於無法指定執行圈數
+whilecode ();
+
 function whilecode () {
     // 總數
     let number = 200;
@@ -119,8 +121,21 @@ function whilecode () {
     // 亂數 (系統產生介於 0 ~ 1 之間)，將小數變為整數 ( 數值要介於 0 ~  含30 ，需乘以 31)，設定為無條件捨去法
     
     let attack;
-    attack = Math.floor(Math.random() * 30);
-    // console.log(attack);
+    let count = 1;
+
+    while (number > 0) {
+        console.log("第" + count + "次");
+        count = count + 1;
+        
+        attack = Math.floor(Math.random() * 30);
+        console.log("亂數為：" + attack);
+
+        // 將計算過後的值，再指定給 number
+        number = number - attack;
+
+        console.log("計算結果：" + number);
+    }
+        
 
 }
 
