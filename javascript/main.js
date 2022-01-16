8099058b176909ff47b8023dcdfe6ba771711e5d
SophiaChungweb <yukimoe728@gmail.com>
2022-01-16 17:41:00 +0800

    20220116_用jquery控版漢堡列的開啟與關閉

--- javascript/main.js
+++ javascript/main.js
@@ -36,11 +36,50 @@ smallnavcon();
 function smallnavcon() {
     // 新增標籤 ul 與 li
     let ul = document.createElement('ul');
-    let li = document.createElement('li');
+    
 
     // 在 smallnav 內新增，宣告變數
-    let SN = document.getElementById('smallnav');
+    let sn = document.getElementById('smallnav');
 
     // 建立 ul 框架，給予 id 名稱
-    SN.appendChild(ul);
+    sn.appendChild(ul);
+    // 賦予屬性 id or class
+    ul.setAttribute("id", "phonenav");  
+
+    let phonenav = document.getElementById('phonenav');
+
+    let navclass = ['about', 'vline', 'shop', 'vline', 'message', 'vline', 'address'];
+    let navcon = ['品牌故事', '︱', '購物說明', '︱', '客服留言', '︱', '門市資訊'];
+    // 新增標籤時，程式的運作方式為：先新增標籤，賦予屬性，再指定新增的外框架(此為一個迴圈)
+    // 迴圈內的數字 7，是因為陣列而產生，故要隨著陣列值的數量而增減
+    for ( let i = 0; i < navclass.length; i++) {
+        let li = document.createElement('li');
+        // 新增屬性
+        li.setAttribute("class", navclass[li]);
+        // ul 內新增 li 清單
+        phonenav.appendChild(li);
+
+        // 新增連結 a
+        let A = document.createElement('a');
+        // 新增屬性 假連結 javascript:;
+        A.setAttribute("href", "javascript:;");
+        // li 內新增 a 連結
+        li.appendChild(A);
+        // 置入 陣列內容
+        A.innerText = navcon[i];
+    }
+}
+
+// menunav();
+// 行動裝置 選單
+function menunav() {
+    let menunav = document.getElementById('menunav');
+    // 隱藏 手機版選單
+    menunav.style.display = "none";
+}
+
+pageload()
+
+function pageload() {
+    $('header').load("../web/header.html");
 }
