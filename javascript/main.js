35ed60b9c047be0fea15f9795339c3e95b50cd03
SophiaChungweb <yukimoe728@email.com>
2022-07-16 21:30:58 +0800

    回歸原來的數據，看效果

--- javascript/main.js
+++ javascript/main.js
@@ -83,3 +83,21 @@ pageload()
 function pageload() {
     $('header').load("../web/header.html");
 }
+
+phonemenushow2();
+
+function phonemenushow2() {
+    // $ jQuery的變數前綴詞
+    // 隱藏選單
+    $('#menunav').hide();
+
+    // 開啟選單
+    $('#menubtn').click( function() {
+        $('#menunav').show();
+    });
+
+    // 關閉選單
+    $('#close').click( function() {
+        $('#menunav').hide();
+    });
+}
